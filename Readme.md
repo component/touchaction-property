742c0b21cf9a7446eac8b7ea74bff50289da8835
Nathan Rajlich <nathan@tootallnate.net>
2013-11-13 12:00:40 -0800

    initial commit

---
+++ Readme.md
@@ -0,0 +1,18 @@
+
+# touchaction-property
+
+  Returns &quot;touchAction&quot;, &quot;msTouchAction&quot;, or null
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install component/touchaction-property
+
+## API
+
+
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,20 @@
+
+/**
+ * Module exports.
+ */
+
+module.exports = touchActionProperty();
+
+/**
+ * Returns "touchAction", "msTouchAction", or null.
+ */
+
+function touchActionProperty(doc) {
+  if (!doc) doc = document;
+  var div = doc.createElement('div');
+  var prop = null;
+  if ('touchAction' in div.style) prop = 'touchAction';
+  else if ('msTouchAction' in div.style) prop = 'msTouchAction';
+  div = null;
+  return prop;
+}
